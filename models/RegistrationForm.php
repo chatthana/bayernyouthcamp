2b5502ab168731736465882c565223d0d5911e53
Chatthana Janethanakarn <chattana.j@gmail.com>
2016-11-14 02:55:25 +0700

    Second Commit

---
+++ models/RegistrationForm.php
@@ -0,0 +1,31 @@
+<?php
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class RegistrationForm extends Model {
+
+  public $firstname;
+  public $lastname;
+  public $email;
+  public $telephone;
+
+  public function rules() {
+    return [
+      [['firstname', 'lastname', 'email', 'telephone'], 'required'],
+      ['email', 'email'],
+    ];
+  }
+
+  public function attributeLabels() {
+    return [
+      'firstname' => 'ชื่อ',
+      'lastname' => 'นามสกุล',
+      'email' => 'อีเมล์',
+      'telephone' => 'เบอร์โทรศัพท์ที่ติดต่อได้'
+    ]
+  }
+
+}
+?>
