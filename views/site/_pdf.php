398b1a2700a52327fcfb0cd3e4a1da83d93ac115
Chatthana Janethanakarn <chattana.j@gmail.com>
2016-11-28 07:20:44 +0700

    Morning Commit

--- views/site/_pdf.php
+++ views/site/_pdf.php
@@ -8,3 +8,15 @@
     <h3>FC Bayern Youth Cup 2017</h3>
   </div>
 </section>
+<section id="content">
+  <div>
+    <p class="col-2"><span>ชื่อ :</span> รุจิรา สุวรรณวาลัย</p>
+    <p class="col-2"><span>ชื่อ (อังกฤษ) :</span> Rujira Suwannawalai</p>
+    <p class="col-2"><span>ชื่อเล่น : อ๋อม</span></p>
+  </div>
+  <div>
+    <p class="col-2"><span>วันเกิด :</span> 23 มกราคม 2002</p>
+    <p class="col-2"><span>ชื่อ (อังกฤษ) :</span> Rujira Suwannawalai</p>
+    <p class="col-2"><span>ชื่อเล่น : อ๋อม</span></p>
+  </div>
+</section>
