758f44995939a9fdec531db02a1ce83fce7a4146
Chatthana Janethanakarn <chattana.j@gmail.com>
2016-12-08 23:16:52 +0700

    Preproduction

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -2,10 +2,36 @@
 
 namespace app\controllers;
 
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
 class AdminController extends \yii\web\Controller
 {
 
     public $layout = 'bootstrap';
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+            'access' => [
+              'class' => AccessControl::className(),
+              'rules' => [
+                  [
+                      'allow' => true,
+                      'roles' => ['@'],
+                  ],
+
+                  // ...
+              ],
+            ],
+        ];
+    }
 
     public function actionIndex()
     {
