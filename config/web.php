67b972de4f16540fa664958d3553710a394f5bac
Chatthana Janethanakarn <chatthana@mol.com>
2016-12-09 21:43:02 +0700

    Preproduction

--- config/web.php
+++ config/web.php
@@ -6,7 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    // 'catchAll' => ['site/offline'],
+    'catchAll' => ['site/offline'],
     // 'aliases' => [
     //   '@uploads' => '@webroot/images/uploads',
     // ],
