14d8a0bc9c5f744ab93d3d2d1f71020a9090a559
Chatthana Jenethanakarn <chattana.j@gmail.com>
2017-02-21 23:27:11 +0700

    Production II

--- views/gallery/index.php
+++ views/gallery/index.php
@@ -24,26 +24,31 @@ $this->registerJs('
 </div>
 <div class="row">
   <div class="col-md-12">
-    <table class="table table-hover table-bordered table-stripped">
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Remark</th>
-        <th>Active</th>
-        <th>Operation</th>
-      </tr>
-      <?php foreach ($galleries as $gallery): ?>
+    <?php if (count($galleries) > 0): ?>
+      <table class="table table-hover table-bordered table-stripped">
         <tr>
-          <td><?php echo $gallery->id; ?></td>
-          <td><?php echo $gallery->name; ?></td>
-          <td><?php echo $gallery->remark; ?></td>
-          <td><?php echo $gallery->active; ?></td>
-          <td>
-            <a href="<?php echo Yii::$app->urlManager->createUrl(['gallery/display', 'id' => $gallery->id]); ?>" class="fa fa-search"></a>
-            <a href="<?php echo Yii::$app->urlManager->createUrl(['gallery/delete', 'id'=> $gallery->id]); ?>" class="fa fa-trash deleter"></a>
-          </td>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Remark</th>
+          <th>Active</th>
+          <th>Operation</th>
         </tr>
-      <?php endforeach; ?>
-    </table>
+        <?php foreach ($galleries as $gallery): ?>
+          <tr>
+            <td><?php echo $gallery->id; ?></td>
+            <td><?php echo $gallery->name; ?></td>
+            <td><?php echo $gallery->remark; ?></td>
+            <td><?php echo $gallery->active; ?></td>
+            <td>
+              <a href="<?php echo Yii::$app->urlManager->createUrl(['gallery/display', 'id' => $gallery->id]); ?>" class="fa fa-search"></a>
+              <a href="<?php echo Yii::$app->urlManager->createUrl(['gallery/delete', 'id'=> $gallery->id]); ?>" class="fa fa-trash deleter"></a>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </table>
+    <?php else : ?>
+      <h1>No gallery found</h1>
+    <?php endif; ?>
+
   </div>
 </div>
