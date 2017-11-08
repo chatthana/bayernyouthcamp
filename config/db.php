de29b68876e175927c7a545c2e7d61b880320ede
Chatthana Janethanakarn <chattana.j@gmail.com>
2017-11-09 02:50:36 +0700

    Add DB config for staging server

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bayern',
+    'dsn' => 'mysql:host=localhost;dbname=bayern_test',
     'username' => 'root',
     'password' => 'secret',
     'charset' => 'utf8',
