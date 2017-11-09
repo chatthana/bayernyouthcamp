e9d5b7a8a3f9c161eb7b9a40b85fc8fc17eaa0d1
Chatthana Janethanakarn <chattana.j@gmail.com>
2017-11-09 23:30:02 +0700

    Fix

--- views/site/news.php
+++ views/site/news.php
@@ -8,16 +8,18 @@ use app\components\ThaiDateHelper;
     <h1>สนามคัดเลือก</h1>
     <div class="region-container">
         <?php foreach ($regions as $region): ?>
-          <div class="each-region">
-            <h3><?php echo $region->name; ?></h3>
-            <?php foreach ($region->arenas as $arena): ?>
-              <div class="each-arena">
-                <p><?php // echo ThaiDateHelper::getThaiDate($arena->reg_date); ?></p>
-                <p><?php  echo $arena->text; ?></p>
-                <p> (สมัครออนไลน์ภายในวันที่ <?php echo ThaiDateHelper::getThaiDate($arena->last_reg_date); ?>)</p>
-              </div>
-            <?php endforeach; ?>
-          </div>
+          <?php if (count($region->arenas) > 0): ?>
+            <div class="each-region">
+              <h3><?php echo $region->name; ?></h3>
+              <?php foreach ($region->arenas as $arena): ?>
+                <div class="each-arena">
+                  <p><?php // echo ThaiDateHelper::getThaiDate($arena->reg_date); ?></p>
+                  <p><?php  echo $arena->text; ?></p>
+                  <p> (สมัครออนไลน์ภายในวันที่ <?php echo ThaiDateHelper::getThaiDate($arena->last_reg_date); ?>)</p>
+                </div>
+              <?php endforeach; ?>
+            </div>
+          <?php endif; ?>
         <?php endforeach; ?>
       </div>
     </div>
