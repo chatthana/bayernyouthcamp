48a30332dae0b531c87316e3fcacade72cf60e27
Chatthana Janethanakarn <chattana.j@gmail.com>
2017-11-09 22:41:22 +0700

    Fix DB config

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=bayern',
     'username' => 'root',
-    'password' => '',
+    'password' => 'secret',
     'charset' => 'utf8',
 ];
